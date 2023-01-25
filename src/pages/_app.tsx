c0029c2914e2f71e8876b3136767667ea52383b8
fellipebastos <fellipebastoswm@gmail.com>
2023-01-25 09:25:28 -0300

    [style] create header cart button

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import type { AppProps } from 'next/app'
 import Image from 'next/image'
 import Link from 'next/link'
+import { Handbag } from 'phosphor-react'
 
 import logoImg from '../assets/logo.svg'
 
 import { globalStyles } from '../styles/global'
-import { Container, Header } from '../styles/pages/app'
+import { CartButton, Container, Header } from '../styles/pages/app'
 
 globalStyles()
 
@@ -16,6 +17,11 @@ export default function App({ Component, pageProps }: AppProps) {
         <Link href="/">
           <Image src={logoImg} alt="" />
         </Link>
+
+        <CartButton type="button" disabled hasItems>
+          <Handbag size={24} />
+          <span>1</span>
+        </CartButton>
       </Header>
 
       <Component {...pageProps} />
--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -13,4 +13,53 @@ export const Header = styled('header', {
   width: '100%',
   maxWidth: 1180,
   margin: '0 auto',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+})
+
+export const CartButton = styled('button', {
+  position: 'relative',
+  padding: '0.75rem',
+
+  border: 0,
+  borderRadius: '6px',
+
+  background: '$gray800',
+  color: '$gray500',
+
+  cursor: 'pointer',
+
+  span: {
+    height: '1.5rem',
+    width: '1.5rem',
+    padding: '0.5rem',
+
+    fontSize: '$xs',
+    fontweight: 'bold',
+    color: '$white',
+    backgroundColor: '$green500',
+    border: '3px solid $gray900',
+    borderRadius: '999px',
+
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: '-7px',
+    right: '-7px',
+  },
+
+  '&:disabled': {
+    cursor: 'not-allowed',
+  },
+
+  variants: {
+    hasItems: {
+      true: {
+        color: '$gray300',
+      }
+    }
+  }
 })
